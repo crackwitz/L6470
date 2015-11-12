0c25fcf5aaa3df631ade8fc60be6573b2e90db69
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2015-11-12 08:54:13 +0100

    examples: command test. for development.

---
+++ examples/command_test/command_test.ino
@@ -0,0 +1,16 @@
+#include <SPI.h>
+#include <L6470.h>
+
+L6470 driver(10, 7, 6);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Serial.println(sizeof(struct abs_pos));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+}
